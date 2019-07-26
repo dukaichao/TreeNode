210c8ffb038390b5859851ed4bcb0a0859fc466d
杜凯超 <2822365215@qq.com>
2019-07-26 22:39:45 +0800

    二叉树层序遍历每层加入集合中

---
+++ levelOrder07_26/levelOrder.java
@@ -0,0 +1,57 @@
+package dkc.leetcode.levelOrder07_26;
+
+import dkc.leetcode.TreeNode;
+
+import java.util.*;
+
+/**
+ * Created With IntelliJ IDEA.
+ * Descriptions:给定一个二叉树，返回其按层次遍历的节点值。 （即逐层地，从左到右访问所有节点）。
+ *
+ * 例如:
+ * 给定二叉树: [3,9,20,null,null,15,7],
+ *
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ * 返回其层次遍历结果：
+ *
+ * [
+ *   [3],
+ *   [9,20],
+ *   [15,7]
+ * ]
+ *
+ * User:Mr.Du
+ * Date:2019/7/26
+ * Time:22:33
+ */
+public class levelOrder {
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> levels = new ArrayList<List<Integer>>();
+        if (root == null) return levels;
+
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.add(root);
+        int level = 0;
+        while ( !queue.isEmpty() ) {
+            // start the current level
+            levels.add(new ArrayList<Integer>());
+
+            int level_length = queue.size();
+            for(int i = 0; i < level_length; ++i) {
+                TreeNode node = queue.remove();
+
+                levels.get(level).add(node.val);
+
+                if (node.left != null) queue.add(node.left);
+                if (node.right != null) queue.add(node.right);
+            }
+            level++;
+        }
+        return levels;
+    }
+}
